9d36a652795e9a1d7a009c15bf2c21b8cfcb7a05
Your User Name <fardoushjahanara@gmail.com>
2022-07-08 10:47:51 +0600

    create fullname input form use to usestate hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 
 const App = () =>{
+const [name,setName] = useState(" ");
+const[fullName,setFullName] = useState ();
+    const inputEvent = (event) =>{
+// console.log(event.target.value);
+setName(event.target.value);
+
+    }
+
+    const onSubmits = () =>{
+setFullName(name );
+    }
     return(
         <>
+            <div className="loginFrom-wrapper">
             <h1> Login Form </h1>
+            <div className="loginForm-inner">
+            <h3>Hello ..! {fullName}</h3>
+
+                <input type="text" placeholder='Enter Your Name'  onChange={inputEvent} value = {name} />
+
+              
+            </div>
+            <button className="jfss-btn" onClick={onSubmits}> Click Me </button>
+            </div>
+
+
         </>
     )
 }
